e4607ce72c57969d5318d65114ef029b98ad331e
Ben Blank <bblank@linkedin.com>
2019-08-02 15:00:45 -0700

    Implement hex_to_bytes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn bytes_to_hex(bytes: Vec<u8>) -> String {
+fn bytes_to_hex(bytes: &Vec<u8>) -> String {
     let mut hex = Vec::new();
 
     for byte in bytes.iter() {
@@ -8,10 +8,19 @@ fn bytes_to_hex(bytes: Vec<u8>) -> String {
     hex.concat()
 }
 
-fn main() {
-    let string = String::from("foo");
-    let bytes = string.as_bytes().to_vec();
-    let hex = bytes_to_hex(bytes);
+fn hex_to_bytes(hex: &str) -> Vec<u8> {
+    // https://stackoverflow.com/a/52992629/46387
+    (0..hex.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16).unwrap())
+        .collect()
+}
 
-    println!("{}", hex);
+fn main() {
+    let string1 = String::from("foo");
+    let bytes1 = string1.as_bytes().to_vec();
+    let hex = bytes_to_hex(&bytes1);
+    let bytes2 = hex_to_bytes(&hex);
+
+    println!("{} {:?} {} {:?}", string1, bytes1, hex, bytes2);
 }
