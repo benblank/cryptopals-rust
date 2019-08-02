020c451189b83345770efaf9d3a468d5a1271e69
Ben Blank <bblank@linkedin.com>
2019-08-02 15:57:13 -0700

    Solve exercise 2.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
+fn xor(message: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
+    assert_eq!(message.len(), key.len());
+
+    (0..message.len()).map(|i| message[i] ^ key[i]).collect()
+}
+
 fn main() {
-    let foo = "foo";
-    let hex_ = hex::encode(&foo);
-    let b64 = base64::encode(&foo);
-
-    println!("{} {} {}", foo, hex_, b64);
-
     let exercise1 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let string = String::from_utf8(hex::decode(&exercise1).unwrap()).unwrap();
     let b64 = base64::encode(&string);
 
-    println!("{}", exercise1);
+    assert_eq!("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", b64);
+
     println!("{}", string);
-    println!("{}", b64);
+
+    let exercise2 = hex::decode("1c0111001f010100061a024b53535009181c").unwrap();
+    let key = hex::decode("686974207468652062756c6c277320657965").unwrap();
+    let xor_ = xor(&exercise2, &key);
+    let b64 = hex::encode(&xor_);
+
+    assert_eq!("746865206b696420646f6e277420706c6179", b64);
+
+    println!("{}", String::from_utf8(xor_).unwrap());
 }
