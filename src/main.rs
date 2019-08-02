d3e6325af6227f6bcd57513d98b4b217f532b90f
Ben Blank <bblank@linkedin.com>
2019-08-02 15:00:29 -0700

    Implement bytes_to_hex.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn bytes_to_hex(bytes: Vec<u8>) -> String {
+    let mut hex = Vec::new();
+
+    for byte in bytes.iter() {
+        hex.push(format!("{:x}", byte));
+    }
+
+    hex.concat()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let string = String::from("foo");
+    let bytes = string.as_bytes().to_vec();
+    let hex = bytes_to_hex(bytes);
+
+    println!("{}", hex);
 }
