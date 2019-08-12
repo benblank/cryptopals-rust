be578e465d567efd315f7de4d6f98e78b844efc3
Ben Blank <bblank@linkedin.com>
2019-08-12 15:11:25 -0700

    Refactor chi-squared calculation and related functions.

--- src/cryptopals.rs
+++ src/cryptopals.rs
@@ -1,14 +1,29 @@
+use std::cmp::Ordering;
 use std::collections::HashMap;
 
-fn chi_squared_etaoin_shrdlu(candidate: &[u8]) -> f64 {
-    let byte_counts = count_bytes(&candidate);
-    let expected_counts = get_expected_counts(candidate.len(), &byte_counts.keys().collect::<Vec<&u8>>());
+fn calculate_chi_squared(actual_counts: &HashMap<u8, f64>, expected_counts: &HashMap<u8, f64>) -> Result<f64, String> {
+    (0u8..=255u8).map(|byte| {
+        let actual = actual_counts.get(&byte).unwrap_or(&0.0);
+        let maybe_expected = expected_counts.get(&byte);
 
-    byte_counts.iter().map(|(byte, &actual)| {
-        let expected = expected_counts.get(&byte).unwrap();
-
-        (actual as f64 - expected).powf(2.0) / expected
-    }).sum()
+        match maybe_expected {
+            Some(expected) => Ok(if actual == expected {
+                // Specifically, this handles the case of expecting 0 and
+                // finding 0, but can short-circuit a little bit of math, too.
+                0.0
+            } else {
+                (actual - expected).powf(2.0) / expected
+            }),
+            None => Err(format!("Expected hash is missing an entry for {}", byte)),
+        }
+    }).fold(Ok(0.0), |sum, result| {
+        match (sum, result) {
+            (Ok(sum), Ok(result)) => Ok(sum + result),
+            (Ok(_), Err(result)) => Err(result),
+            (Err(err), Ok(_)) => Err(err),
+            (Err(err1), Err(err2)) => Err(format!("{}\n{}", err1, err2)),
+        }
+    })
 }
 
 fn count_bytes(message: &[u8]) -> HashMap<u8, usize> {
@@ -21,61 +36,79 @@ fn count_bytes(message: &[u8]) -> HashMap<u8, usize> {
     counts
 }
 
-fn get_expected_counts(message_len: usize, bytes: &[&u8]) -> HashMap<u8, f64> {
-    let mut expected_counts = HashMap::new();
-
-    for &&byte in bytes {
-        expected_counts.insert(byte, get_letter_frequency(byte) * message_len as f64);
-    }
-
-    expected_counts
-}
-
-fn get_letter_frequency(byte: u8) -> f64 {
+fn get_byte_frequency(byte: u8) -> f64 {
+    // FIXME: should really sum to 1.0
     match byte {
-        b'A' | b'a' => 8.55,
-        b'B' | b'b' => 1.60,
-        b'C' | b'c' => 3.16,
-        b'D' | b'd' => 3.87,
-        b'E' | b'e' => 12.10,
-        b'F' | b'f' => 2.18,
-        b'G' | b'g' => 2.09,
-        b'H' | b'h' => 4.96,
-        b'I' | b'i' => 7.33,
-        b'J' | b'j' => 0.22,
-        b'K' | b'k' => 0.81,
-        b'L' | b'l' => 4.21,
-        b'M' | b'm' => 2.53,
-        b'N' | b'n' => 7.17,
-        b'O' | b'o' => 7.47,
-        b'P' | b'p' => 2.07,
-        b'Q' | b'q' => 0.10,
-        b'R' | b'r' => 6.33,
-        b'S' | b's' => 6.73,
-        b'T' | b't' => 8.94,
-        b'U' | b'u' => 2.68,
-        b'V' | b'v' => 1.06,
-        b'W' | b'w' => 1.83,
-        b'X' | b'x' => 0.19,
-        b'Y' | b'y' => 1.72,
-        b'Z' | b'z' => 0.11,
+        b'A' | b'a' => 0.0855,
+        b'B' | b'b' => 0.0160,
+        b'C' | b'c' => 0.0316,
+        b'D' | b'd' => 0.0387,
+        b'E' | b'e' => 0.1210,
+        b'F' | b'f' => 0.0218,
+        b'G' | b'g' => 0.0209,
+        b'H' | b'h' => 0.0496,
+        b'I' | b'i' => 0.0733,
+        b'J' | b'j' => 0.0022,
+        b'K' | b'k' => 0.0081,
+        b'L' | b'l' => 0.0421,
+        b'M' | b'm' => 0.0253,
+        b'N' | b'n' => 0.0717,
+        b'O' | b'o' => 0.0747,
+        b'P' | b'p' => 0.0207,
+        b'Q' | b'q' => 0.0010,
+        b'R' | b'r' => 0.0633,
+        b'S' | b's' => 0.0673,
+        b'T' | b't' => 0.0894,
+        b'U' | b'u' => 0.0268,
+        b'V' | b'v' => 0.0106,
+        b'W' | b'w' => 0.0183,
+        b'X' | b'x' => 0.0019,
+        b'Y' | b'y' => 0.0172,
+        b'Z' | b'z' => 0.0011,
 
-        // HACK: Without a frequency for spaces, chi-squared can't tell the
-        // difference between uppercase and lowercase.
-        // b' ' => 12.10,
+        // FIXME: digits?
 
-        _ => 0.0001,
+        // Spaces are much more likely...
+        b' ' => 0.1210,
+
+        // ... than other printable characters.
+        b'\t' | b'\r' | b'\n' | b'!'..=b'~' => 0.0010,
+
+        // Non-printables shouldn't appear at all.  (Causes a chi-squared of NaN.)
+        _ => 0.0,
     }
 }
 
-pub fn get_single_byte_key(message: &[u8]) -> u8 {
-    let mut chi_squareds = (0u8..255u8)
-        .map(|byte| (byte, chi_squared_etaoin_shrdlu(&xor_single_byte_key(message, byte))))
-        .collect::<Vec<(u8, f64)>>();
+pub fn get_single_byte_key(message: &[u8]) -> Result<u8, String> {
+    let expected_counts = (0u8..=255u8)
+        .map(|byte| (byte, message.len() as f64 * get_byte_frequency(byte)))
+        .collect();
+    let mut chi_squareds = (0u8..=255u8)
+        .map(|byte| {
+            let candidate = xor_single_byte_key(&message, byte);
+            let actual_counts = count_bytes(&candidate)
+                .iter()
+                .map(|(&byte, &count)| (byte, count as f64))
+                .collect::<HashMap<u8, f64>>();
 
-    chi_squareds.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
+            (byte, calculate_chi_squared(&actual_counts, &expected_counts))
+        })
+        .collect::<Vec<(u8, Result<f64, String>)>>();
 
-    chi_squareds[0].0
+    // Fall back to Ordering::Greater if the comparison is None (e.g. involves
+    // NaN).
+    chi_squareds.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Greater));
+
+    match chi_squareds
+        .iter()
+        .filter(|(_, result)| result.is_ok())
+        .collect::<Vec<&(u8, Result<f64, String>)>>()
+        .len() {
+        // A lack of candidates is caused by a bad `expected_counts`, caused by
+        // problems with get_byte_frequency.
+        0 => Err("No candidates found (this is a bug)".to_string()),
+        _ => Ok(chi_squareds[0].0),
+    }
 }
 
 pub fn xor_repeating_key(message: &[u8], key: &[u8]) -> Result<Vec<u8>, String> {
@@ -94,7 +127,55 @@ pub fn xor_single_byte_key(message: &[u8], key: u8) -> Vec<u8> {
 mod tests {
     use super::*;
 
-    // chi_squared_etaoin_shrdlu will be refactored into something easier to test.
+    // Modified from the assert_eq! definition.
+    macro_rules! assert_partial_eq {
+        ($left:expr, $right:expr) => ({
+            match (&$left, &$right) {
+                (left_val, right_val) => {
+                    let maybe_ordering = (*left_val).partial_cmp(&*right_val);
+
+                    if maybe_ordering == None || maybe_ordering != Some(Ordering::Equal) {
+                        panic!(r#"assertion failed: `(left == right)`
+    left: `{:?}`,
+    right: `{:?}`"#, &*left_val, &*right_val)
+                    }
+                }
+            }
+        });
+    }
+
+    #[test]
+    fn calculate_chi_squared_same() {
+        let counts = (0u8..=255u8)
+            .map(|byte| (byte, byte as f64 + 1.0))
+            .collect::<HashMap<u8, f64>>();
+
+        assert_partial_eq!(Ok(0.0), calculate_chi_squared(&counts, &counts));
+    }
+
+    #[test]
+    fn calculate_chi_squared_missing_expected_key() {
+        assert!(calculate_chi_squared(&HashMap::new(), &HashMap::new()).is_err());
+    }
+
+    #[test]
+    fn calculate_chi_squared_works() {
+        let actual_counts = (0u8..=255u8).map(|byte| (byte, match byte {
+            b'a' => 1.0,
+            b'b' => 2.0,
+            b'c' => 1.0,
+            _ => 1.0,
+        })).collect::<HashMap<u8, f64>>();
+
+        let expected_counts = (0u8..=255u8).map(|byte| (byte, match byte {
+            b'a' => 1.0,
+            b'b' => 1.0,
+            b'c' => 2.0,
+            _ => 1.0,
+        })).collect::<HashMap<u8, f64>>();
+
+        assert_partial_eq!(Ok(1.5), calculate_chi_squared(&actual_counts, &expected_counts));
+    }
 
     #[test]
     fn count_bytes_empty_message() {
@@ -113,9 +194,7 @@ mod tests {
         assert_eq!(expected, count_bytes(&vec![b'a', b'b', b'c', b'd', b'a']));
     }
 
-    // get_expected_counts will be refactored into something easier to test.
-
-    // get_letter_frequency is a "lookup table" and doesn't need testing?
+    // get_byte_frequency is a "lookup table" and doesn't need testing?
 
     // get_single_byte_key will be tested once chi_squared_etaoin_shrdlu is refactored.
 
@@ -143,10 +222,7 @@ mod tests {
 
     #[test]
     fn xor_repeating_key_bad_key() {
-        match xor_repeating_key(&vec![], &vec![]) {
-            Ok(_) => panic!("Expected an empty key to result in an error."),
-            Err(_) => (),
-        }
+        assert!(xor_repeating_key(&vec![], &vec![]).is_err());
     }
 
     #[test]
--- src/exercise3.rs
+++ src/exercise3.rs
@@ -4,7 +4,7 @@ const INPUT: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783
 
 pub fn run_and_print() {
     let bytes = hex::decode(INPUT).unwrap();
-    let key = get_single_byte_key(&bytes);
+    let key = get_single_byte_key(&bytes).unwrap();
 
     assert_eq!(key, b'X');  // Panic rather than printing a possibly-invalid string.
     println!("{}", String::from_utf8(xor_single_byte_key(&bytes, key)).unwrap());
